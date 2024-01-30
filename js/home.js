24006ad5f81653e09d7efd60259c84591c5b0354
parachuter <parachuter@gmail.com>
2024-01-30 19:43:36 +0800

    完善主页面卡片点击跳转功能

--- js/home.js
+++ js/home.js
@@ -25,3 +25,26 @@ function setTargetCharacter(){
         window.location.href = './character.html?' + params.toString();
     })
 }
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    /**
+     * 为下方介绍卡片添加点击跳转事件
+     */
+    var characterCard = document.querySelector('.character-card');
+    characterCard.addEventListener('click', function() {
+        // 无，这里本来就希望用户通过搜字跳转查字页面，而非随意查
+    });
+    var wordbookCard = document.querySelector('.wordbook-card');
+    wordbookCard.addEventListener('click', function() {
+        window.location.href = './wordbook.html';
+    })
+    var examineCard = document.querySelector('.examine-card');
+    examineCard.addEventListener('click', function() {
+        window.location.href = './examine.html';
+    })
+    var phraseCard = document.querySelector('.phrase-card');
+    phraseCard.addEventListener('click', function() {
+        window.location.href = './phrase.html';
+    })
+})
