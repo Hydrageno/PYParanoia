40eb1634b514f21a0c48a2f8945b444ada4ed9ef
parachuter <parachuter@gmail.com>
2024-02-01 20:50:36 +0800

    为固定词语添加机器读音

--- js/phrase.js
+++ js/phrase.js
@@ -30,10 +30,26 @@ function searchPhrase(){
                 var contentTemplate = document.createElement('div');
                 contentTemplate.classList.add('content-template');
                 contentTemplate.innerHTML = `
-                    <div class="word"><span class="prefix">词语：</span>${data['words'][i]}</div>
-                    <div class="word-pinyin"><span class="prefix">拼音：</span>${data['words_pinyin'][i]}</div>
-                    <div class="text"><span class="prefix">词语解释：</span>${data['texts'][i]}</div>
-                    <div class="text-pinyin"><span class="prefix">解释拼音：</span>${data['texts_pinyin'][i]}</div>
+                    <div class="word">
+                        <span class="prefix">词语：</span>
+                        ${data['words'][i]}
+                    </div>
+                    <div class="word-pinyin">
+                        <span class="prefix">拼音：</span>
+                        ${data['words_pinyin'][i]}
+                        <span class="hidden-content">${data['words'][i]}</span>
+                        <button type="button" class="machine-read-button" title="念出来" onclick="machineRead(event)">念</button>
+                    </div>
+                    <div class="text">
+                        <span class="prefix">词语解释：</span>
+                        ${data['texts'][i]}
+                    </div>
+                    <div class="text-pinyin">
+                        <span class="prefix">解释拼音：</span>
+                        ${data['texts_pinyin'][i]}
+                        <span class="hidden-content">${data['texts'][i]}</span>
+                        <button type="button" class="machine-read-button" title="念出来" onclick="machineRead(event)">念</button>
+                    </div>
                     <br>
                 `
                 writableRegion.appendChild(contentTemplate);
@@ -68,3 +84,23 @@ function redirectWordbookExamine()
     window.location.href = './examine.html';
 }
 
+
+var backEndSpeaked = false; // 阻止一次性过多访问，导致崩溃
+function machineRead(event)
+{
+    /**
+     * 功能：念出来！
+     */
+    if(backEndSpeaked == true)return;
+    backEndSpeaked = true;
+    var button = event.target;
+    var previousSibling = button.previousElementSibling; // 使用 previousElementSibling 获取前一个兄弟元素节点
+    previousSibling.style.display = 'inline'; // 将前一个兄弟元素节点显示出来
+    fetch('http://127.0.0.1:5000/machine_read/' + previousSibling.textContent)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data['message']);
+        backEndSpeaked = false; //直至后端念完后，才允许下一个点击
+    })
+    previousSibling.style.display = 'none'; // 恢复前一个兄弟元素节点的隐藏状态
+}
